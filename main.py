3e476f26a6da1041b69e04b00f3e03cd3a240c18
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-03-03 20:05:55 +0000

    254. Tkinter Layout Managers: pack(), place() and grid()

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from tkinter import *
 window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
-
-my_label = Label(text="My Label", font=('Arial', 24, 'bold'))
-my_label.pack()
+window.config(padx=100, pady=100)
 
 
 def button_clicked():
@@ -14,9 +12,20 @@ def button_clicked():
     my_label.config(text=input_box.get())
 
 
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# Label
+my_label = Label(text="My Label", font=('Arial', 24, 'bold'))
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
+# Button
+button = Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+# Button 2
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+# Entry
 input_box = Entry(width=10)
-input_box.pack()
+input_box.grid(column=3, row=2)
 window.mainloop()
