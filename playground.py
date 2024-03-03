6511df297d5adae2542093a92702ed85bf319b5f
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-03-03 18:34:04 +0000

    251. **kwargs: Many Keyword Arguments

--- playground.py
+++ playground.py
@@ -3,3 +3,26 @@ def add(*args):
 
 
 add(1, 2, 3, 4, 5)
+
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(n=2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kwargs):
+        # self.make = kwargs["make"]
+        # self.model = kwargs["model"] # Code will have error if model is not initialized
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+        # get() returns None if the element is not present
+
+
+car1 = Car(make="Nissan")
+print(car1.model)
